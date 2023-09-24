53591f89a0d386706f5dee3f2badf073cd1b15f7
kapanaitat <90142672+kapanaitat@users.noreply.github.com>
2023-09-24 21:49:21 +0900

    Create custom_interfaces.sh

---
+++ custom_interfaces.sh
@@ -0,0 +1,119 @@
+#! /bin/sh.
+
+## careting a package
+## making a directory for msg files
+ros2 pkg create --build-type ament_cmake more_interfaces
+mkdir more_interfaces/msg
+
+# making a msg file AddressBook.msg inside more_interfaces/msg
+uint8 PHONE_TYPE_HOME=0
+uint8 PHONE_TYPE_WORK=1
+uint8 PHONE_TYPE_MOBILE=2
+
+string first_name
+string last_name
+string phone_number
+uint8 phone_type
+
+## building a msg file 
+# adding into package.xml
+<buildtool_depend>rosidl_default_generators</buildtool_depend>
+
+<exec_depend>rosidl_default_runtime</exec_depend>
+
+<member_of_group>rosidl_interface_packages</member_of_group>
+
+## in CMakeLists.txt
+find_package(rosidl_default_generators REQUIRED)
+set(msg_files
+  "msg/AddressBook.msg"
+)
+rosidl_generate_interfaces(${PROJECT_NAME}
+  ${msg_files}
+)
+ament_export_dependencies(rosidl_default_runtime)
+
+
+## using intrface from the same package
+# in more_interfaces/src creating a file publish_address_book.cpp with the content
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "more_interfaces/msg/address_book.hpp"
+
+using namespace std::chrono_literals;
+
+class AddressBookPublisher : public rclcpp::Node
+{
+public:
+  AddressBookPublisher()
+  : Node("address_book_publisher")
+  {
+    address_book_publisher_ =
+      this->create_publisher<more_interfaces::msg::AddressBook>("address_book", 10);
+
+    auto publish_msg = [this]() -> void {
+        auto message = more_interfaces::msg::AddressBook();
+
+        message.first_name = "John";
+        message.last_name = "Doe";
+        message.phone_number = "1234567890";
+        message.phone_type = message.PHONE_TYPE_MOBILE;
+
+        std::cout << "Publishing Contact\nFirst:" << message.first_name <<
+          "  Last:" << message.last_name << std::endl;
+
+        this->address_book_publisher_->publish(message);
+      };
+    timer_ = this->create_wall_timer(1s, publish_msg);
+  }
+
+private:
+  rclcpp::Publisher<more_interfaces::msg::AddressBook>::SharedPtr address_book_publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<AddressBookPublisher>());
+  rclcpp::shutdown();
+
+  return 0;
+}
+
+
+
+## building the publisher
+# making a new target in the CMakeLists.txt
+find_package(rclcpp REQUIRED)
+
+add_executable(publish_address_book src/publish_address_book.cpp)
+ament_target_dependencies(publish_address_book rclcpp)
+
+install(TARGETS
+    publish_address_book
+  DESTINATION lib/${PROJECT_NAME})
+
+## link against the interface
+rosidl_get_typesupport_target(cpp_typesupport_target
+  ${PROJECT_NAME} rosidl_typesupport_cpp)
+
+target_link_libraries(publish_address_book "${cpp_typesupport_target}")
+
+
+
+
+## trying it out
+cd ~/ros2_ws
+colcon build --packages-up-to more_interfaces
+source install/local_setup.bash
+ros2 run more_interfaces publish_address_book
+
+
+# in a new terminal 
+source install/setup.bash
+ros2 topic echo /address_book
+
