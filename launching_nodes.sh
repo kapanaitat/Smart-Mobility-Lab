195ad3a4913c48780d208d6bf63396a4a68df2a2
kapanaitat <90142672+kapanaitat@users.noreply.github.com>
2023-09-24 20:38:41 +0900

    Create launching_nodes.sh

---
+++ launching_nodes.sh
@@ -0,0 +1,14 @@
+#! /bin/sh.
+
+
+## running a launch file in a new terminal
+ros2 launch turtlesim multisim.launch.py
+
+
+## running in the second terminal
+ros2 topic pub  /turtlesim1/turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.8}}"
+
+## running in the third terminal
+ros2 topic pub  /turtlesim2/turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -1.8}}"
+
+
